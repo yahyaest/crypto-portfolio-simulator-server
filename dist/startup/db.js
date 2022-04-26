0f116128e14e29ef49c337351a925484335407e0
Yahya <machatyahya@gmail.com>
2022-04-26 18:27:05 +0100

    Prepare For Deployment

--- dist/startup/db.js
+++ dist/startup/db.js
@@ -7,7 +7,10 @@ exports.db = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const db = () => {
     mongoose_1.default
-        .connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kf0nx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+        .connect(process.env.app_db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
         .then(() => console.log("Connected to MongoDB..."))
         .catch((err) => console.log("Could not connect to MongoDB... ", err));
 };
--- src/startup/db.ts
+++ src/startup/db.ts
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 export const db = () => {
   mongoose
-    .connect(
-      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kf0nx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect(process.env.app_db as string, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("Connected to MongoDB..."))
     .catch((err: Error) =>
       console.log("Could not connect to MongoDB... ", err)
